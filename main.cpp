673de75d1545144e46a4dcf7646a8cf99baead78
ngohoangkhang30 <24520748@gm.uit.edu.vn>
2024-09-28 18:34:04 +0700

    9_28_2024

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "123";
-    return 0;
+   float a,b,c,delta;
+   cin >> a;
+   cin >> b;
+   cin >> c;
+   delta =pow(b,2)-4*a*c;
+   if (delta>0 && a!=0 ) {
+	cout<<"PT co hai nghiem phan biet:"<<endl;
+	cout<<"x1 = "<< (-b+sqrt(delta))/(2*a)<<endl;
+	cout<<"x2 = "<< (-b-sqrt(delta))/(2*a);
+	return 0;
+   }
+   if(delta=0 && a!=0 ){
+	cout<<"PT co nghiem kep: x1 = x2 = "<<-b/(2*a);
+	return 0;
+   }
+   else {cout << "PTVN";}
 }
